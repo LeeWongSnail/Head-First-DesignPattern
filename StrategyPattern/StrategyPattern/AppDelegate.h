b04043e2ec678865c5099667be74332953ae3f64
LeeWongSnail <@wangli_0632@163.com>
2016-09-12 18:29:29 +0800

    strategy pattern init

---
+++ StrategyPattern/StrategyPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StrategyPattern
+//
+//  Created by LeeWong on 16/9/12.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StrategyPattern/StrategyPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrategyPattern
+//
+//  Created by LeeWong on 16/9/12.
+//  Copyright © 2016年 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
